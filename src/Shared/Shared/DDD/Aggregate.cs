21ccab3468e16f4eaac5d499b4e5ab5ef30b581f
biyonik <ahmet.altun60@gmail.com>
2024-08-31 02:21:56 +0300

    Developed shared domain for tactical ddd abstraction (entity, aggregate, events)

---
+++ src/Shared/Shared/DDD/Aggregate.cs
@@ -0,0 +1,15 @@
+
+namespace Shared.DDD;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = [.. _domainEvents];
+        _domainEvents.Clear();
+        return dequeuedEvents;
+    }
+    public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+}
